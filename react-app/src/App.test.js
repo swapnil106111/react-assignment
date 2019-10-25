682393fb7259369f508aa8b7995d44048da5596d
swapnil106111 <swap106111@gmail.com>
2019-10-25 18:19:15 +0530

    more test cases added

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -56,4 +56,104 @@ describe("Restaurants component", () => {
     errorState = wrapper.state().selectedData.errors.people;
     expect(errorState).toBe("Please enter number of people between 1 to 10");
   });
+
+  it("click on next buttonby setting mealtype and without setting no of people", () => {
+    const wrapper = shallow(<Restaurants />);
+
+    let errorState = wrapper.state().selectedData.errors.meal;
+    expect(errorState).toBe(undefined);
+    errorState = wrapper.state().selectedData.errors.people;
+    expect(errorState).toBe(undefined);
+    const selectedData = {
+      mealType: "Dinner",
+      people: "",
+      restaurant: "",
+      dishSelection: [{ name: "", servings: 1, validServings: true }],
+      errors: {}
+    };
+    wrapper.setState({ selectedData: selectedData });
+    const nextBtn = wrapper.find(".next");
+    nextBtn.simulate("click");
+    errorState = wrapper.state().selectedData.errors.meal;
+    expect(errorState).toBe(undefined);
+    errorState = wrapper.state().selectedData.errors.people;
+    expect(errorState).toBe("Please enter number of people between 1 to 10");
+  });
+
+  it("click on next buttonby without setting mealtype and by setting no of people", () => {
+    const wrapper = shallow(<Restaurants />);
+
+    let errorState = wrapper.state().selectedData.errors.meal;
+    expect(errorState).toBe(undefined);
+    errorState = wrapper.state().selectedData.errors.people;
+    expect(errorState).toBe(undefined);
+    const selectedData = {
+      mealType: "",
+      people: 5,
+      restaurant: "",
+      dishSelection: [{ name: "", servings: 1, validServings: true }],
+      errors: {}
+    };
+    wrapper.setState({ selectedData: selectedData });
+    const nextBtn = wrapper.find(".next");
+    nextBtn.simulate("click");
+    errorState = wrapper.state().selectedData.errors.meal;
+    expect(errorState).toBe("Please select a meal");
+    errorState = wrapper.state().selectedData.errors.people;
+    expect(errorState).toBe(undefined);
+  });
+
+  it("if number no of people is between 1 to 10", () => {
+    const wrapper = shallow(<Restaurants />);
+    const selectedData = {
+      mealType: "Dinner",
+      people: 5,
+      restaurant: "",
+      dishSelection: [{ name: "", servings: 1, validServings: true }],
+      errors: {}
+    };
+    wrapper.setState({ selectedData: selectedData });
+    const nextBtn = wrapper.find(".next");
+    nextBtn.simulate("click");
+    let errorState = wrapper.state().selectedData.errors.meal;
+    expect(errorState).toBe(undefined);
+    errorState = wrapper.state().selectedData.errors.people;
+    expect(errorState).toBe(undefined);
+  });
+
+  it("if number no of people are more than 10", () => {
+    const wrapper = shallow(<Restaurants />);
+    const selectedData = {
+      mealType: "Dinner",
+      people: 11,
+      restaurant: "",
+      dishSelection: [{ name: "", servings: 1, validServings: true }],
+      errors: {}
+    };
+    wrapper.setState({ selectedData: selectedData });
+    const nextBtn = wrapper.find(".next");
+    nextBtn.simulate("click");
+    let errorState = wrapper.state().selectedData.errors.meal;
+    expect(errorState).toBe(undefined);
+    errorState = wrapper.state().selectedData.errors.people;
+    expect(errorState).toBe("Please enter number of people between 1 to 10");
+  });
+
+  it("if meal type is selected and valid no of people entered and then click on next button", () => {
+    const wrapper = shallow(<Restaurants />);
+    const selectedData = {
+      mealType: "Dinner",
+      people: 10,
+      restaurant: "",
+      dishSelection: [{ name: "", servings: 1, validServings: true }],
+      errors: {}
+    };
+    wrapper.setState({ selectedData: selectedData });
+    const nextBtn = wrapper.find(".next");
+    nextBtn.simulate("click");
+    let errorState = wrapper.state().selectedData.errors.meal;
+    expect(errorState).toBe(undefined);
+    errorState = wrapper.state().selectedData.errors.people;
+    expect(errorState).toBe(undefined);
+  });
 });
